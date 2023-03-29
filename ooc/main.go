448b9c2817fbc2c5a6b05d0407644d4c74190e8e
Aleksandr Rybolovlev <rybolovlev.nl@gmail.com>
2023-03-29 17:34:56 +0200

    Update main.go

--- ooc/main.go
+++ ooc/main.go
@@ -1,3 +1,20 @@
+/*
+Copyright 2016 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Note: the example only works with the code within the same release/branch.
 package main
 
 import (
@@ -12,6 +29,14 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
+	//
+	// Uncomment to load all auth plugins
+	// _ "k8s.io/client-go/plugin/pkg/client/auth"
+	//
+	// Or uncomment to load specific auth plugins
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/azure"
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
 )
 
 func main() {
@@ -37,7 +62,8 @@ func main() {
 	// CREATE
 	o := &nodev1.RuntimeClass{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: "runtime-class",
+			// Name: "runtime-class",
+			GenerateName: "runtime-class-",
 		},
 		Handler: "runc",
 	}
@@ -50,16 +76,16 @@ func main() {
 	fmt.Println("Created Runtime Class:", rc)
 
 	// Read
-	rc, err = clientset.NodeV1().RuntimeClasses().Get(context.TODO(), o.GetObjectMeta().GetName(), metav1.GetOptions{})
+	rcRead, err := clientset.NodeV1().RuntimeClasses().Get(context.TODO(), rc.Name, metav1.GetOptions{})
 	if err != nil {
 		fmt.Printf("Failed to read the Runtime Class object: %v\n", err)
 		os.Exit(1)
 	}
 
-	fmt.Println("Read Runtime Class:", rc)
+	fmt.Println("Read Runtime Class:", rcRead)
 
 	// Delete
-	err = clientset.NodeV1().RuntimeClasses().Delete(context.TODO(), o.GetObjectMeta().GetName(), metav1.DeleteOptions{})
+	err = clientset.NodeV1().RuntimeClasses().Delete(context.TODO(), rc.Name, metav1.DeleteOptions{})
 	if err != nil {
 		fmt.Printf("Failed to delete the Runtime Class object: %v\n", err)
 		os.Exit(1)
